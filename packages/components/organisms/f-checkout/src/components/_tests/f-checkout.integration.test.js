933416501c0ee2bde301f89b5040875ec99a20e1
Joshua Ng <joshua.ng@just-eat.com>
2021-06-01 12:11:00 +0100

    Created initial integration test format.

---
+++ packages/components/organisms/f-checkout/src/components/_tests/f-checkout.integration.test.js
@@ -0,0 +1,93 @@
+// import httpModule from '@justeat/f-http';
+// import {
+//     mount
+// } from '@vue/test-utils';
+// import flushPromises from 'flush-promises';
+
+// import Checkout from '../Checkout.vue';
+// import EventNames from '../../event-names';
+// import { defaultCheckoutState } from './helpers/setup';
+
+// const {
+//     mockFactory,
+//     httpVerbs
+// } = httpModule;
+
+// const updateCheckoutUrl = 'http://localhost/updatecheckout';
+// const getCheckoutUrl = 'http://localhost/checkout';
+// const checkoutAvailableFulfilmentUrl = 'http://localhost/checkout/fulfilment';
+// const loginUrl = 'http://localhost/login';
+// const createGuestUrl = 'http://localhost/createguestuser';
+// const getBasketUrl = 'http://localhost/getbasket';
+// const getAddressUrl = 'http://localhost/getaddress';
+// const placeOrderUrl = 'http://localhost/placeorder';
+// const paymentPageUrlPrefix = 'http://localhost/paymentpage';
+// const getGeoLocationUrl = 'http://localhost/geolocation';
+
+// const propsData = {
+//     updateCheckoutUrl,
+//     getCheckoutUrl,
+//     loginUrl,
+//     checkoutAvailableFulfilmentUrl,
+//     createGuestUrl,
+//     getBasketUrl,
+//     getAddressUrl,
+//     placeOrderUrl,
+//     paymentPageUrlPrefix,
+//     getGeoLocationUrl
+// };
+
+// const setGuestFormFieldValues = wrapper => {
+//     wrapper.find('[data-test-id="formfield-guest-first-name-input"]').setValue(defaultCheckoutState.customer.firstName);
+//     wrapper.find('[data-test-id="formfield-guest-last-name-input"]').setValue(defaultCheckoutState.customer.lastName);
+//     wrapper.find('[data-test-id="formfield-guest-email-input"]').setValue(defaultCheckoutState.customer.emailAddress);
+//     wrapper.find('[data-test-id="formfield-mobile-number-input"]').setValue(defaultCheckoutState.customer.mobileNumber);
+//     wrapper.find('[data-test-id="formfield-address-line-1-input"]').setValue(defaultCheckoutState.address.line1);
+//     wrapper.find('[data-test-id="formfield-address-line-2-input"]').setValue(defaultCheckoutState.address.line2);
+//     wrapper.find('[data-test-id="formfield-address-locality-input"]').setValue(defaultCheckoutState.address.locality);
+//     wrapper.find('[data-test-id="formfield-address-postcode-input"]').setValue(defaultCheckoutState.address.postcode);
+// };
+
+// const setFormFieldValues = wrapper => {
+//     wrapper.find('[data-test-id="formfield-mobile-number-input"]').setValue(defaultCheckoutState.customer.mobileNumber);
+//     wrapper.find('[data-test-id="formfield-address-line-1-input"]').setValue(defaultCheckoutState.address.line1);
+//     wrapper.find('[data-test-id="formfield-address-line-2-input"]').setValue(defaultCheckoutState.address.line2);
+//     wrapper.find('[data-test-id="formfield-address-locality-input"]').setValue(defaultCheckoutState.address.locality);
+//     wrapper.find('[data-test-id="formfield-address-postcode-input"]').setValue(defaultCheckoutState.address.postcode);
+// };
+
+
+// describe('Checkout API service', () => {
+//     let wrapper;
+
+//     beforeEach(() => {
+//         const div = document.createElement('div');
+//         document.body.appendChild(div);
+//         wrapper = mount(Checkout, {
+//             propsData,
+//             attachTo: div
+//         });
+//         console.log(`Some wrapper thing here = ${wrapper}`);
+//         setGuestFormFieldValues(wrapper);
+//     });
+
+//     // afterEach(() => {
+//     //     wrapper.destroy();
+//     //     jest.clearAllMocks();
+//     // });
+
+//     it('responds with 201 when request is made with valid details', async () => {
+//         // Arrange
+//         console.log(`Some text here = ${wrapper}`);
+//         mockFactory.setupMockResponse(httpVerbs.GET, propsData.createGuestUrl, defaultCheckoutState, 201);
+//         console.log(`The Mocks are here = ${mockFactory}`);
+//         console.log(`Some Checkout Get Success Here = ${EventNames.CheckoutGetSuccess}`);
+
+//         // Act
+//         // await wrapper.vm.submitOrder();
+//         await flushPromises();
+
+//         // Assert
+//         expect(wrapper.emitted(EventNames.CheckoutGetSuccess).length).toBe(1);
+//     });
+// });
